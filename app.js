931620938ff83189ea82eede0b87c57b8aa43bfd
ARBUCHELI <radio_seattle@hotmail.com>
2023-05-05 11:11:21 -0500

    add new page content, remove page content

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const card = document.querySelector('#ga-7096a2 .card__content--h5');
 
 card.textContent = 'Example of Updating Page Content';
+
+const container = document.createElement('span');
+
+container.textContent = ', right now!';
+
+const mainHeading = document.querySelector('h1');
+
+mainHeading.appendChild(container);
+
+const cardContainer = document.querySelector('.hero__module');
+
+const firstCard = cardContainer.firstElementChild
+
+cardContainer.removeChild(firstCard);
+
