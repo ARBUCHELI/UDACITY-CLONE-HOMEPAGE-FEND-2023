e09f649c4dde53da4112a3c75b8bd30fc98e6755
ARBUCHELI <radio_seattle@hotmail.com>
2023-05-15 12:22:44 -0500

    feat: avoid too many events - refactoring the number of event listeners

--- app.js
+++ app.js
@@ -60,3 +60,19 @@ document.addEventListener('click', function (event) {
 	document.body.appendChild(myCustomDiv);	
 */
 
+const myCustomDiv = document.createElement('div');
+
+function respondToTheClick() {
+    console.log('A paragraph was clicked.');
+}
+
+for (let i = 1; i <= 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.textContent = 'This is paragraph number ' + i;
+
+    newElement.addEventListener('click', respondToTheClick);
+
+    myCustomDiv.appendChild(newElement);
+}
+
+document.body.appendChild(myCustomDiv);
