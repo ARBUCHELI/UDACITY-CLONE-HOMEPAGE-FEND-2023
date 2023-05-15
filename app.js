f8658241c027760f3b43cafcd276d11684cff160
ARBUCHELI <radio_seattle@hotmail.com>
2023-05-15 12:19:10 -0500

    feat: avoid too many events - avoid too many events example

--- app.js
+++ app.js
@@ -44,4 +44,19 @@ document.addEventListener('click', function (event) {
     console.log(event);
 })
 
+/*const myCustomDiv = document.createElement('div');
+
+	for (let i = 1; i <= 200; i++) {
+    		const newElement = document.createElement('p');
+    		newElement.textContent = 'This is paragraph number ' + i;
+
+    		newElement.addEventListener('click', function respondToTheClick(evt) {
+        		console.log('A paragraph was clicked.');
+    		});
+
+    		myCustomDiv.appendChild(newElement);
+	}
+
+	document.body.appendChild(myCustomDiv);	
+*/
 
